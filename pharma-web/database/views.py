b15043711a005b69aabec80a53f8c0fa7091fc32
LAugustoZaave <lucas@zaave.com.br>
2024-11-28 03:22:02 -0300

    Algumas telas

--- pharma-web/database/views.py
+++ pharma-web/database/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.template import loader
+from .models import clientes
+from django.http import HttpResponse
+
 
 # Create your views here.
+def database(request):
+    clientes_login = clientes.objects.all().values()
+    template = loader.get_template('login.html')
+    context = {
+        'clientes': clientes_login,
+    }
+
+    return HttpResponse(template.render(context, request))
+
+def cadastro(request):
+    template = loader.get_template('cadastro.html')
+
+    return HttpResponse(template.render(request=request))
+
+def paginaInicial(request):
+    template = loader.get_template('pagina_inicial.html')
+    
+    return HttpResponse(template.render(request=request))
